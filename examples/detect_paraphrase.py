052bab827e5d18f8a2171c9a4c783ef441224ad4
Teemu Vahtola <teemu.vahtola@helsinki.fi>
2024-04-30 11:37:29 +0300

    Updated detect_paraphrase.py

--- examples/detect_paraphrase.py
+++ examples/detect_paraphrase.py
@@ -13,6 +13,7 @@ import datasets
 from datasets import Dataset
 from datasets import DatasetDict
 
+sys.path.append("src/")
 from swag_transformers.swag_bert import SwagBertForSequenceClassification
 from swag_transformers.trainer_utils import SwagUpdateCallback
 
@@ -28,16 +29,23 @@ def main():
     parser.add_argument("--train_data", type=str, help="Path to training dataset (json)")
     parser.add_argument("--eval_data", type=str, help="Path to validation dataset (json)")
     parser.add_argument("--test_data", type=str, help="Path to test dataset (json)")
+    parser.add_argument("--cache", type=str, default="./tmp", help="Temporary directory for storing models and data downloaded from HF.")
     args = parser.parse_args()
 
     tokenizer = transformers.AutoTokenizer.from_pretrained(args.base_model)
     model = transformers.AutoModelForSequenceClassification.from_pretrained(args.base_model, num_labels=3)
     swag_model = SwagBertForSequenceClassification.from_base(model)
 
-    # train_data = [json.loads(line) for line in itertools.islice(args.train_data, args.limit_training)]
-    train_data = [json.loads(line) for line in args.train_data]
-    eval_data = [json.loads(line) for line in args.eval_data]
-    test_data = [json.loads(line) for line in args.test_data]
+    def read_data(data_path):
+        data = []
+        with open(data_path, "r") as file:
+            for line in file:
+                data.append(json.loads(line))
+        return data
+
+    train_data = read_data(args.train_data)
+    eval_data = read_data(args.eval_data)
+    test_data = read_data(args.test_data)
 
     train_dataset = Dataset.from_list(train_data)
     eval_dataset = Dataset.from_list(eval_data)
@@ -50,6 +58,13 @@ def main():
     })
 
     if args.limit_training:
+        dataset = DatasetDict({
+            "train": dataset["train"].select(range(args.limit_training)),
+            "validation": dataset["validation"],
+            "test": dataset["test"]
+        })
+
+    if args.limit_training:
         dataset = dataset.select(range(args.limit_training))
 
     def tokenize_dataset(dataset):
@@ -59,19 +74,22 @@ def main():
         return processed
 
     training_args = transformers.TrainingArguments(
-        output_dir="save_folder",
+        output_dir=args.save_folder,
         learning_rate=2e-5,
-        per_device_train_batch_size=4,
-        per_device_eval_batch_size=4,
+        per_device_train_batch_size=32,
+        per_device_eval_batch_size=32,
         num_train_epochs=3,
+        seed=42,
+        save_steps=500,
+        save_total_limit=1,
     )
 
     processed_train = dataset["train"].map(
-        tokenize_dataset, batched=True, remove_columns=dataset.column_names)
+        tokenize_dataset, batched=True, remove_columns=dataset["train"].column_names)
     processed_eval = dataset["validation"].map(
-        tokenize_dataset, batched=True, remove_columns=dataset.column_names)
+        tokenize_dataset, batched=True, remove_columns=dataset["validation"].column_names)
     processed_test = dataset["test"].map(
-        tokenize_dataset, batched=True, remove_columns=dataset.column_names)
+        tokenize_dataset, batched=True, remove_columns=dataset["test"].column_names)
 
     data_collator = transformers.DataCollatorWithPadding(tokenizer=tokenizer)
     trainer = transformers.Trainer(
