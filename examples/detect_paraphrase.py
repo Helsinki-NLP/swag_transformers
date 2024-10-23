c3c590b4049f96520c912a8fd70ae3a6318fa450
Teemu Vahtola <teemu.vahtola@helsinki.fi>
2024-10-23 14:03:26 +0300

    update detect_paraphrase.py

--- examples/detect_paraphrase.py
+++ examples/detect_paraphrase.py
@@ -245,7 +245,7 @@ def main():
         save_total_limit=1,
         metric_for_best_model="accuracy",
         greater_is_better=True,
-        load_best_model_at_end=False,
+        load_best_model_at_end=True,
     )
 
     processed_train = dataset["train"].map(
@@ -274,12 +274,14 @@ def main():
     )
     trainer.train()
     trainer.save_model(os.path.join(args.save_folder, "final_base"))
+    trainer.save_state()
 
     # Save the full model + tokenizer + training arguments (similar to trainer.save_model)
     final_out = os.path.join(args.save_folder, "final")
     swag_model.save_pretrained(final_out)
     trainer.tokenizer.save_pretrained(final_out)
     torch.save(trainer.args, os.path.join(final_out, transformers.trainer.TRAINING_ARGS_NAME))
+    trainer.state.save_to_json(os.path.join(final_out, "trainer_state.json"))
 
 
 if __name__ == '__main__':
